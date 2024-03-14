8c4ef88443bccd4bd6619450ebdf6fa22de00a53
Andrew Lia <709686@student.Inholland.nl>
2024-03-14 07:21:06 +0100

    Added comments to Lecturer and Person to be uniform with Room model

--- SomerenModel/Lecturer.cs
+++ SomerenModel/Lecturer.cs
@@ -2,9 +2,9 @@
 {
     public class Lecturer : Person
     {
-        public int Id { get; set; }
-        public int Age { get; set; }
-        public string RoomNumber { get; set; }
+        public int Id { get; set; }                 // lecturerId, database primary key
+        public int Age { get; set; }                
+        public string RoomNumber { get; set; }      // room_number, foreign key referencing Rooms table
 
         public Lecturer(int id, int age, string roomNumber, string firstName, string lastName, string phoneNumber)
             :base(firstName, lastName, phoneNumber)
--- SomerenModel/Person.cs
+++ SomerenModel/Person.cs
@@ -8,10 +8,10 @@ namespace SomerenModel
 {
     public abstract class Person
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string FullName { get { return FirstName + " " + LastName; } }
+        public string FirstName { get; set; }                                   
+        public string LastName { get; set; }                                    
+        public string PhoneNumber { get; set; }                                 
+        public string FullName { get { return FirstName + " " + LastName; } }   //calculated property to combine FirstName and LastName
         protected Person(string firstName, string lastName, string phoneNumber)
         {
             FirstName = firstName;
